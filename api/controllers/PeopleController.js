e89573000a0723d980e86abbc3f803b5c83cd26b
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-14 11:19:30 -0300

    CRUD - delete and update

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -36,6 +36,45 @@ class PeopleController{
             return response.status(500).json(err.message);
         }
     }
+
+    static async updatePeople(request, response){
+        const {id} = request.params;
+        const updatePerson = request.body;
+
+        try{
+            await db.people.update(updatePerson, {
+                where: {
+                    id: Number(id)
+                }
+            });
+
+            const person = await db.people.findOne({
+                where: {
+                    id: Number(id)
+                }
+            });
+
+            return response.status(200).json(person);
+        } catch (err){
+            return response.status(500).json(err.message);
+        }
+    }
+
+    static async deletePeople(request, response){
+        const {id} = request.params;
+
+        try{
+            await db.people.destroy({
+                where: {
+                    id: Number(id)
+                }
+            });
+
+            return response.status(200).json("Person deleted!");
+        } catch (err){
+            return response.status(500).json(err.message);
+        }
+    }
 }
 
 module.exports = PeopleController;
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/people', PeopleController.getAllPeople);
 router.get('/people/:id', PeopleController.getPeopleById);
 router.post('/people', PeopleController.createPeople);
+router.post('/people/:id', PeopleController.updatePeople);
+router.delete('/people/:id', PeopleController.deletePeople);
 
 module.exports = router;
