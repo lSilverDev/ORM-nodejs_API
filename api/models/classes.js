cf715389fdc07e4c49bf184cccd22a3444668cd2
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-15 15:11:59 -0300

    pattern

--- api/models/classes.js
+++ api/models/classes.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class classes extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       classes.hasMany(models.enrollments, {
         foreignKey: 'class_id'
--- api/models/enrollments.js
+++ api/models/enrollments.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class enrollments extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       enrollments.belongsTo(models.people, {
         foreignKey: 'student_id'
